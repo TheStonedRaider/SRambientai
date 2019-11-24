f8d9fabd68957220f576b36b79826b7f86be67ca
The Stoned Raider <joebopie@googlemail.com>
2019-11-24 04:32:19 +0000

    Update config.lua

--- config.lua
+++ config.lua
@@ -2,20 +2,24 @@ Config                            = {}
    --- 
 Config.MaxPerPlayer = 15 --------- How many cars are spawned per player, 
 ------ this is halfed when near another player whis a minimum of 2 car per player. 
-Config.Minimum = 1
-Config.peddensity = 0.5     ------------ Density of peds minimum = 0.0 max = 1.0
------- this is halfed when near another player 
-Config.parkedcars = 0.5        ----------- Density of parked cars  minimum = 0.0 max = 1.0
------- this is halfed when near another player 
+Config.Minimum = 1 -------- How many cars are spawned per player
 
-Config.DisToHalf = 250   ---- Distance 
+Config.DisToHalf = 250   ---- Distance to another player to reduce the MaxPerPlayer count.
+
+
+Config.peddensity = 0.5     ------------ Density of peds minimum = 0.0 max = 1.0
+------ this devided by the amount of players near  you
+Config.parkedcars = 0.5        ----------- Density of parked cars  minimum = 0.0 max = 1.0
+------ this devided by the amount of players near  you
+
+
 
 
 Config.minSpawndis = 130   ----- Minimum spawning distance from player
 
 Config.MaxSpawndis = 300   ----- Maximum spawning distance from player 
 
-Config.minSpawndis2 = 150 ----- Minimum spawning distance from  other players (best kept the same as Config.minSpawndis
+Config.minSpawndis2 = 130 ----- Minimum spawning distance from  other players (best kept the same as Config.minSpawndis
 
 Config.Deldis = 325 ----- Distance that cars will be set as no longer needed ! Always keep this higher than - Config.MaxSpawndis 
 
@@ -23,7 +27,7 @@ Config.Deldis = 325 ----- Distance that cars will be set as no longer needed ! A
 -------------------Debug mode!
 debugrestart = false   --- Set TRUE if restarting script when server is running 
 
-debugmode =  true  --- shows the search and spawn spots 
+debugmode =  false  --- shows the search and spawn spots 
 
 
 	 citycars = { --  this is the format you need to put in for your possible locations.
